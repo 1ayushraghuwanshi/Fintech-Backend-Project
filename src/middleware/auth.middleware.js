b919748768f469494a4dc327214e1921717cc0a8
1ayushraghuwanshi <ayushraghu1c@email.com>
2026-03-27 09:36:47 +0530

    Initial commit for Backed  Project

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,77 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../model/user.model.js")
+const tokenBlackListModel = require("../model/blackList.model.js")
+
+async function authMiddleware(req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+    const isBlackListed = await tokenBlackListModel.findOne({ token })
+    if(isBlackListed) {
+        return res.status(401).json({
+            message: "Unathorized access, token is invalid"
+        })
+    }
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.userId)
+
+        req.user = user
+
+        return next()
+
+    } catch (err) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+}
+
+async function authSystemUserMiddleware(req, res, next){
+   const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ] 
+
+     if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+   const isBlackListed = await tokenBlackListModel.findOne({ token })
+    if(isBlackListed) {
+        return res.status(401).json({
+            message: "Unathorized access, token is invalid"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser")
+        if(!user.systemUser){
+            return res.status(403).json({
+                message: "Forbidden access, not a sysytem user"
+            })
+        }
+        req.user = user
+        return next()
+    }
+    catch(err){
+        return res.status(401).json({
+            message: "Unauthorized access, token is Invalid"
+        })
+    }
+}
+
+
+module.exports ={
+  authMiddleware,
+  authSystemUserMiddleware
+}
